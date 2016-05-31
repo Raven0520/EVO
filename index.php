423ddf533f26e524e480e0bf109cc7bdb585ab73
Raven0520 <181444127@qq.com>
2016-06-01 00:30:08 +0800

    初始化后台代码  02

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Raven
+ * Date: 2016/5/22
+ * Time: 21:57
+ */
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+define('APP_DEBUG',True);
+//定义静态化页面生成路径
+define('HTML_PATH','./');
+// 定义应用目录
+define('APP_PATH','./Application/');
+
+// 引入ThinkPHP入口文件
+require 'ThinkPHP/ThinkPHP.php';
